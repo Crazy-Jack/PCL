d623a75d70bdf4a2fc773b06e872915e286ee4b4
Tianqin Li <jacklitianqin@gmail.com>
2021-05-05 16:09:19 +0900

    hubert2

--- script/hubert2/run_model_imagenet_weakcheckedbycluster_hubert2.sh
+++ script/hubert2/run_model_imagenet_weakcheckedbycluster_hubert2.sh
@@ -8,29 +8,29 @@
 #SBATCH --exclude compute-2-9
 #SBATCH -o /results/tianqinl/train_related/log/sbatch_log/%j.out
 
-bz=512;
+bz=1536;
 
-CUDA_VISIBLE_DEVICES=$3 python3 ../main_moco_cluster_checkbyweak_new.py /data/yaohungt/ILSVRC/Data/CLS-LOC/ \
+CUDA_VISIBLE_DEVICES=$3 python3 ~/PCL/main_moco_weak_checkbycluster.py /data/yaohungt/ILSVRC/Data/CLS-LOC \
 -a resnet50 \
 --lr 0.4 \
---epochs 50 \
 --batch-size $bz \
 --temperature 0.2 \
 --mlp --aug-plus --cos \
 --dist-url 'tcp://localhost:10002' --multiprocessing-distributed --world-size 1 --rank 0 \
---exp-dir /data/yaohungt/ILSVRC/Results_imagenet/imagenet_all/moco_cluster_corrected_by_weaksupcon_new/gran_$1/bz_$bz_num_cluster_$2 \
---warmup-epoch 0 \
+--exp-dir /data/yaohungt/ILSVRC/Results_imagenet/imagenet_all/moco_weakcon_correctbyclustering/gran_$1/bz_$bz_num_cluster_$2 \
+--warmup-epoch 10 \
 --data-root train \
 --save-epoch 5 \
 --perform-cluster-epoch 1 \
---workers 10 \
+--workers 16 \
 --pcl-r 128 \
---threshold 0.05 \
 --num-cluster $2 \
 --latent-class imagenet_all \
 --meta-data-train meta_data_train.csv \
 --gran-lvl $1 \
---launch-eval-epoch 10000 \
+--epochs 100 \
+--launch-eval-epoch 1000 \
+--resume $4 \
 #--resume $3 \
 # --resume /results/tianqinl/train_related/imagenet/target_100/moco_cluster/checkpoint_0194.pth.tar
 # --resume /results/tianqinl/train_related/imagenet/target_100/checkpoint_0099.pth.tar \
